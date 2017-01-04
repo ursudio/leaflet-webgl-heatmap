a95d2481c5b39e3f0da89686b06705411f2106de
bozdoz <howaboutben@gmail.com>
2017-01-04 15:04:36 -0400

    publish.sh

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSION=$(node --eval "console.log(require('./package.json').version);")
+
+# publish master branch
+gulp build
+git commit -am "v$VERSION"
+git tag v$VERSION -f
+git push --tags -f
+git push
